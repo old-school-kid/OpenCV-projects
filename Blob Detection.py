d7b28982cf6edfb3cc38e66c2d137b5adcd3593c
AMANKANOJIYA <aman.kanojiya4203@gmail.com>
2021-06-30 23:13:51 +0530

    Some change

--- Blob Detection.py
+++ Blob Detection.py
@@ -3,18 +3,19 @@ import cv2
 import numpy as np;
  
 # Read image
-image = cv2.imread("images/Sunflowers.jpg")
+image = cv2.imread("images/hand_spot.jpg")
  
 # Set up the detector with default parameters.
 detector = cv2.SimpleBlobDetector_create()
  
 # Detect blobs.
+# provides with all the points with blob
 keypoints = detector.detect(image)
- 
+
 # Draw detected blobs as red circles.
 blank = np.zeros((1,1)) 
-blobs = cv2.drawKeypoints(image, keypoints, blank, (255,0,0),
-                                      cv2.DRAW_MATCHES_FLAGS_DEFAULT)
+blobs = cv2.drawKeypoints(image, keypoints, blank, (0,0,255),cv2.DRAW_MATCHES_FLAGS_DEFAULT)
+
 # Show keypoints
 cv2.imshow("Blobs", blobs)
 cv2.waitKey(0)
